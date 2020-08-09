b3986dfb9978b6ccefd4c805fbbfdd8296106064
Miriam <miriam.haenle@gmail.com>
2020-08-09 21:47:52 +0200

    Improve header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,7 @@ export default function Header({ titleText }) {
 
 const StyledHeader = styled.header`
   margin: 0;
-  background: lightpink;
   padding: 10px;
-  color: hotpink;
   font-family: var(--headlineFont);
   text-align: center;
   font-size: 2rem;
